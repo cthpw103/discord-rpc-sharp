d32b27be717b0d68059472885ced9921f10aa16e
cth103 <cthdev@email.com>
2017-11-10 17:17:28 +0000

    ah

---
+++ Controller.cs
@@ -0,0 +1,90 @@
+using System;
+using RPC;
+
+[System.Serializable]
+public event EventHandler<EventArgs> DiscordJoinEvent; //merci jeuxjeux20
+
+[System.Serializable]
+public event EventHandler<EventArgs> DiscordSpectateEvent; 
+
+[System.Serializable]
+public event EventHandler<DiscordRpc.JoinRequest> DiscordJoinRequestEvent;  //Marche STP.
+
+public class Controller : MonoBehaviour
+{
+    public RichPresence presence;
+    public string applicationId;
+    public string optionalSteamId;
+    public UnityEngine.Events.UnityEvent onConnect;
+    public UnityEngine.Events.UnityEvent onDisconnect;
+    public DiscordJoinEvent onJoin;
+    public DiscordJoinEvent onSpectate;
+    public DiscordJoinRequestEvent onJoinRequest;
+
+    EventHandlers handlers;
+
+    public void ReadyCallback()
+    {
+        onConnect.Invoke();
+    }
+
+    public void DisconnectedCallback(int errorCode, string message)
+    {
+        onDisconnect.Invoke();
+    }
+
+    public void ErrorCallback(int errorCode, string message)
+    {
+    }
+
+    public void JoinCallback(string secret)
+    {
+        onJoin.Invoke(secret);
+    }
+
+    public void SpectateCallback(string secret)
+    {
+        onSpectate.Invoke(secret);
+    }
+
+    public void RequestCallback(DiscordRpc.JoinRequest request)
+    {
+      
+        onJoinRequest.Invoke(request);
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        RunCallbacks();
+    }
+
+    void OnEnable()
+    {
+        //Modifie
+        callbackCalls = 0;
+
+        handlers = new DiscordRpc.EventHandlers();
+        handlers.readyCallback = ReadyCallback;
+        handlers.disconnectedCallback += DisconnectedCallback;
+        handlers.errorCallback += ErrorCallback;
+        handlers.joinCallback += JoinCallback;
+        handlers.spectateCallback += SpectateCallback;
+        handlers.requestCallback += RequestCallback;
+        DiscordRpc.Initialize(applicationId, ref handlers, true, optionalSteamId);
+    }
+
+    void OnDisable()
+    {
+        //Modifie
+        DiscordRpc.Shutdown();
+    }
+
+    void OnDestroy()
+    {
+
+    }
+}
